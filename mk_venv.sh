2bed32b1e5051a0b638ac1a445a6a0d7d9c965fe
HasnainRaza026 <hasnainraza0026@gmail.com>
2024-07-31 23:30:30 +0500

    This is the automated commit

--- mk_venv.sh
+++ mk_venv.sh
@@ -1,6 +1,6 @@
 # Make python virtual enviroment automatically
 # Create the virtual environment
-python -m venv myenv
+python -m venv myenv # python or Python3
 
 # On macOS/Linux:
 source myenv/bin/activate
